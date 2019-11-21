32745ef34519e2f6fa5638284d9acd7be34156cb
NotDestroyer <40222729+NotDestroyer@users.noreply.github.com>
2019-11-21 11:51:35 -0500

    Update teamk.py

--- teamk.py
+++ teamk.py
@@ -1,5 +1,8 @@
 from turtle import *
+from random import*
+
 k = Turtle()
+
 colormode(255)
 
 def reminder():
@@ -8,7 +11,38 @@ def reminder():
   # this will help to keep track of who created each function
   return True
   
+from turtle import *
+from random import *
 
+def drawSeaweed():
+  #by gargaar
+  for i in range(5):
+    k.goto(100,-200)
+    k.fillcolor("green")
+    k.pendown()
+    k.begin_fill()
+    k.setheading(180)
+    for i in range(5): 
+      for i in range(90): 
+        k.forward(.25)
+        k.right(1)
+      k.forward(20)
+      for i in range(90):
+        k.forward(.25)
+        k.right(1)
+      for i in range(90):
+        k.forward(.25)
+        k.left(1)
+      k.forward(20)
+      for i in range(90):
+        k.forward(.25)
+        k.left(1)
+    k.hideturtle()
+    k.end_fill()
+
+update()
+
+drawSeaweed()    
 
 
 update()
