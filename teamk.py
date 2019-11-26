1f9f366921598c4f9cfb4f407f984ea3ee2ffc8d
Katherina-dev <58043660+Katherina-dev@users.noreply.github.com>
2019-11-26 11:53:37 -0500

    Update teamk.py

--- teamk.py
+++ teamk.py
@@ -108,8 +108,177 @@ def drawMan():
     k.circle(30)
     update()
  
+ben = Turtle()
+
+def HotAirBalloon():
+
+  #made by Katherina
+  
+    colormode(255)
+
+    def colour():
+        r = randint(0, 255)
+        g = randint(0, 255)
+        b = randint(0, 255)
+        return (r,g,b)
+
+    tracer(10)
+
+    def balloon():
+
+        ben.fillcolor(colour())
+        ben.begin_fill()
+        for i in range(405):
+            ben.forward(2)
+            ben.left(1)
+        ben.end_fill()
+
+    def basket():
+        ben.right(135)
+        ben.forward(100)
+        ben.fillcolor(colour())
+        ben.begin_fill()
+        ben.left(90)
+        ben.forward(20)
+        ben.right(135)
+        ben.forward(90)
+        ben.right(45)
+        ben.forward(60)
+        ben.right(45)
+        ben.forward(90)
+        ben.right(135)
+        ben.forward(180)
+        ben.end_fill()
+        ben.backward(173)
+        ben.left(90)
+        ben.forward(100)
+
+    def OrbOfHeat():
+        ben.fillcolor("orange")
+        ben.begin_fill()
+        for i in range(360):
+            ben.left(1)
+            ben.forward(0.15)
+        ben.end_fill()
+
+    def fireholder():
+        ben.right(135)
+        for i in range(30):
+            ben.forward(2)
+            ben.left(1)
+        ben.right(75)
+        ben.forward(25)
+        ben.left(90)
+        ben.forward(35)
+        OrbOfHeat()
+        ben.forward(35)
+        ben.left(90)
+        ben.forward(35)
 
 
+    balloon()
+    basket()
+    fireholder()
+
+    update()
+
+ #all colour functions made by Katherina ahaha
+    
+  def randRed():
+    #Made by Katherina
+    r1 = randint(127, 255)
+    g1 = randint(0, 63)
+    b1 = randint(0, 63)
+
+    return (r1, g1, b1)
+
+def randOrange():
+    #Made by Katherina
+    r2 = randint(192, 255)
+    g2 = randint(63, 127)
+    b2 = randint(0, 63)
+
+    return (r2, g2, b2)
+
+def randYellow():
+    #Made by Katherina
+    r3 = randint(192, 255)
+    g3 = r3
+    b3 = randint(0, 63)
+
+    return (r3, g3, b3)
+
+def randGreen():
+    #Made by Katherina
+    r4 = randint(63, 127)
+    g4 = randint(192, 255)
+    b4 = randint(63, 127)
+
+    return (r4, g4, b4)
+
+def randBlue():
+    #Made by Katherina
+    r5 = randint(0, 63)
+    g5 = randint(0, 63)
+    b5 = randint(192, 255)
+
+    return (r5, g5, b5)
+
+def randViolet():
+    #Made by Katherina
+    r6 = randint(63, 127)
+    g6 = randint(0, 63)
+    b6 = randint(192, 255)
+
+    return (r6, g6, b6)
+
+tracer(20)
+
+def squares(colour):
+    #Made by Katherina
+    def square():
+        ben.setheading(90)
+        ben.fillcolor(colour)
+        ben.begin_fill()
+        for i in range(4):
+            ben.forward(32)
+            ben.right(90)
+        ben.right(90)
+        ben.forward(32)
+        ben.end_fill()
+    square()
+
+update()
+
+def colourValues:
+    #Made by Katherina
+    red = randRed()
+    orange = randOrange()
+    yellow = randYellow()
+    green = randGreen()
+    blue = randBlue()
+    violet = randViolet()
+
+    squares(red)
+    squares(orange)
+    squares(yellow)
+    squares(green)
+    squares(blue)
+    squares(violet)
+
+    print("Random red :")
+    print(red)
+    print("Random orange :")
+    print(orange)
+    print("Random yellow :")
+    print(yellow)
+    print("Random green :")
+    print(green)
+    print("Random blue :")
+    print(blue)
+    print("Random violet :")
+    print(violet)
+    
 drawCircle(100,100,55,"red")
 
 drawBalloon()
